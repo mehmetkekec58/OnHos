5e5d3c6a7c64d82ebc4707a7f94bee2aac9d6058
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-02-11 10:31:16 +0300

    11 02 2022

--- DataAccess/Concrete/EntityFramework/Context/MySqlContext.cs
+++ DataAccess/Concrete/EntityFramework/Context/MySqlContext.cs
@@ -13,7 +13,7 @@ namespace DataAccess.Concrete.EntityFramework.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = "server=web08.limonhost.net;user=mehmetke_met;password=benimMYSQLsifrem12;database=mehmetke_wp";
+            var connectionString = "server=;user=;password=;database=";
             var serverVersion = new MySqlServerVersion(new Version(4, 9, 7));
             optionsBuilder.UseMySql(connectionString,serverVersion);
         }
